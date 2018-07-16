718f7d5501aeaa1f21d8643ebbaf3155925ee559
Nikolai Plath <der.el.kuku@gmail.com>
2018-07-16 10:17:18 -0500

    Add a version suitable for module loaders

---
+++ dist/mloader-methods.js
@@ -0,0 +1,1 @@
+module.exports={g11n3t:function(a,b){return g11n.translate(a,b)},g11n4t:function(a,b,c,d){return g11n.translatePlural(a,b,c,d)}};
---
+++ src/mloader-methods.js
@@ -0,0 +1,29 @@
+module.exports = {
+    /**
+     * Small multilanguaging function =;).
+     *
+     * Also includes sprintf() functionality if more parameters are supplied.
+     *
+     * @param original   string The original string to translate
+     * @param parameters object Replacement parameters.
+     *
+     * @returns string The translated string or the original if not found.
+     */
+    g11n3t: function(original, parameters) {
+        return g11n.translate(original, parameters);
+    },
+
+    /**
+     * Small multilanguaging pluralisation function =;).
+     *
+     * @param singular   string  Singular form of text to translate.
+     * @param plural     string  Plural form of text to translate.
+     * @param count      integer The number of items.
+     * @param parameters object  Replacement parameters.
+     *
+     * @return string Translated text.
+     */
+    g11n4t: function(singular, plural, count, parameters) {
+        return g11n.translatePlural(singular, plural, count, parameters);
+    }
+};
